6b26c6042e02acc3779cb049b941ffad1f6a0a15
Nandsgit <70517841+Nandsgit@users.noreply.github.com>
2021-06-15 10:01:33 +0545

    Create Foothill.java

---
+++ a6/src/Foothill.java
@@ -0,0 +1,70 @@
+
+import java.text.*;
+import java.util.*;
+
+//------------------------------------------------------
+public class Foothill
+{ 
+   // -------  main --------------
+   public static void main(String[] args) throws Exception
+   {
+      FHtree<String> sceneTree = new FHtree<String>();
+      FHtreeNode<String> tn;
+      
+      // create a scene in a room
+      tn = sceneTree.addChild(null, "room");
+
+      // add three objects to the scene tree
+      sceneTree.addChild(tn, "Lily the canine");
+      sceneTree.addChild(tn, "Miguel the human");
+      sceneTree.addChild(tn, "table");
+      // add some parts to Miguel
+      tn = sceneTree.find("Miguel the human");
+
+      // Miguel's left arm
+      tn = sceneTree.addChild(tn, "torso");
+      tn = sceneTree.addChild(tn, "left arm");
+      tn =  sceneTree.addChild(tn, "left hand");
+      sceneTree.addChild(tn, "thumb");
+      sceneTree.addChild(tn, "index finger");
+      sceneTree.addChild(tn, "middle finger");
+      sceneTree.addChild(tn, "ring finger");
+      sceneTree.addChild(tn, "pinky");
+
+      // Miguel's right arm
+      tn = sceneTree.find("Miguel the human");
+      tn = sceneTree.find(tn, "torso", 0);
+      tn = sceneTree.addChild(tn, "right arm");
+      tn =  sceneTree.addChild(tn, "right hand");
+      sceneTree.addChild(tn, "thumb");
+      sceneTree.addChild(tn, "index finger");
+      sceneTree.addChild(tn, "middle finger");
+      sceneTree.addChild(tn, "ring finger");
+      sceneTree.addChild(tn, "pinky");
+
+      // add some parts to Lily
+      tn = sceneTree.find("Lily the canine");
+      tn = sceneTree.addChild(tn, "torso");
+      sceneTree.addChild(tn, "right front paw");
+      sceneTree.addChild(tn, "left front paw");
+      sceneTree.addChild(tn, "right rear paw");
+      sceneTree.addChild(tn, "left rear paw");
+      sceneTree.addChild(tn, "spare mutant paw");
+      sceneTree.addChild(tn, "wagging tail");
+
+      // add some parts to table
+      tn = sceneTree.find("table");
+      sceneTree.addChild(tn, "north east leg");
+      sceneTree.addChild(tn, "north west leg");
+      sceneTree.addChild(tn, "south east leg");
+      sceneTree.addChild(tn, "south west leg");
+
+      sceneTree.display();
+      
+      sceneTree.remove("spare mutant paw");
+      sceneTree.remove("Miguel the human");
+      sceneTree.remove("an imagined higgs boson");
+      
+      sceneTree.display();
+   }
+}
